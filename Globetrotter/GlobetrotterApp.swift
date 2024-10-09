ebd1f7026c18c34287d301c6b79087e57f561025
Daneo Van Overloop <7900887+daneov@users.noreply.github.com>
2024-10-09 17:42:49 +0200

    Initial Commit

---
+++ Globetrotter/GlobetrotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobetrotterApp.swift
+//  Globetrotter
+//
+//  Created by Daneo Van Overloop on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GlobetrotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlobetrotterTests/GlobetrotterTests.swift
@@ -0,0 +1,15 @@
+//
+//  GlobetrotterTests.swift
+//  GlobetrotterTests
+//
+//  Created by Daneo Van Overloop on 09/10/2024.
+//
+
+@testable import Globetrotter
+import Testing
+
+struct GlobetrotterTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
